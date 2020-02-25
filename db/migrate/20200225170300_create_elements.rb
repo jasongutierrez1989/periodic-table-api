f9cf1ad6464751e7cedb54d3a9c40dc095de9fbe
jasongutierrez1989 <jasongutierrez1989@gmail.com>
2020-02-25 11:44:36 -0800

    created controller and model for elements and categories. created tables for both and input seed data

---
+++ db/migrate/20200225170300_create_elements.rb
@@ -0,0 +1,13 @@
+class CreateElements < ActiveRecord::Migration[6.0]
+  def change
+    create_table :elements do |t|
+      t.string :name
+      t.string :symbol
+      t.integer :atomic_number
+      t.float :weight
+      t.integer :category_id
+
+      t.timestamps
+    end
+  end
+end
