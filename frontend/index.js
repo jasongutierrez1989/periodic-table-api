cce15cf8092177c7f6c050d01afea3e3c6fe55a0
jasongutierrez1989 <jasongutierrez1989@gmail.com>
2020-05-24 14:54:37 -0700

    basic layout of table

--- frontend/index.js
+++ frontend/index.js
@@ -11,14 +11,36 @@ let elementsFetch = fetch(`http://localhost:3000/elements`)
 async function convertArray() {
   elementsArray = new Array(await elementsFetch);
   for (let i = 0; i < 118; i++) {
+    let grid = document.getElementById('container');
     let textnode = document.createTextNode(elementsArray[0][i].symbol + " ");
     let textnode2 = document.createTextNode(elementsArray[0][i].atomic_number);
+    textnode.id = "symbol";
     let newDiv = document.createElement("div");
-    newDiv.className = "element-" + (i + 1);
+    newDiv.className = "element";
+    newDiv.id = elementsArray[0][i].atomic_number;
     newDiv.appendChild(textnode);
     newDiv.appendChild(textnode2);
-    document.body.appendChild(newDiv);
+    grid.appendChild(newDiv);
+
+    addBlankDivs(i, 0, 16);
+    addBlankDivs(i, 3, 10);
+    addBlankDivs(i, 11, 10);
   }
 }
+
+function addBlankDivs(a, b, c) {
+  if (a == b) {
+    let count = 0;
+    while (count < c) {
+      let grid = document.getElementById('container');
+      let newDiv = document.createElement("div");
+      newDiv.className = "blank";
+      grid.appendChild(newDiv);
+      count ++;
+    }
+  }
+}
+
+//python -m SimpleHTTPServer
 
 convertArray();
