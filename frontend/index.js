a8352b15f1d0419705e80a22910e4ea96e16cac6
jasongutierrez1989 <jasongutierrez1989@gmail.com>
2020-07-01 19:10:21 -0700

    table layout and indices

--- frontend/index.js
+++ frontend/index.js
@@ -10,6 +10,9 @@ let elementsFetch = fetch(`http://localhost:3000/elements`)
 
 async function convertArray() {
   elementsArray = new Array(await elementsFetch);
+
+  addIndexNum(18, "col");
+  addIndexNum(7, "row");
   for (let i = 0; i < 118; i++) {
     let grid = document.getElementById('container');
     let textnode = document.createTextNode(elementsArray[0][i].symbol + " ");
@@ -17,7 +20,7 @@ async function convertArray() {
     textnode.id = "symbol";
     let newDiv = document.createElement("div");
     newDiv.className = "element";
-    newDiv.id = elementsArray[0][i].atomic_number;
+    newDiv.id = 'e' + elementsArray[0][i].atomic_number;
     newDiv.appendChild(textnode);
     newDiv.appendChild(textnode2);
     grid.appendChild(newDiv);
@@ -25,6 +28,25 @@ async function convertArray() {
     addBlankDivs(i, 0, 16);
     addBlankDivs(i, 3, 10);
     addBlankDivs(i, 11, 10);
+    addBlankDivs(i, 56, 1);
+    addBlankDivs(i, 87, 1);
+    addBlankDivs(i, 118, 21);
+  }
+
+  addBlankDivs(1, 1, 21);
+}
+
+function addIndexNum(a, b) {
+  let i = 0;
+  while (i < a) {
+    let grid = document.getElementById('container');
+    let newDiv = document.createElement("div");
+    let textnode = document.createTextNode(i+1);
+    textnode.id = "index";
+    newDiv.appendChild(textnode);
+    newDiv.className = b + "-" + (i+1);
+    grid.appendChild(newDiv);
+    i ++;
   }
 }
 
